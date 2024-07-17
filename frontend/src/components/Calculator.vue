bc1f68f20c965e662c8f0fa6edade5f14f4a6630
Josh Zak <joshuazak19@gmail.com>
2024-07-16 18:29:09 -0700

    Started calculator page

---
+++ frontend/src/components/Calculator.vue
@@ -0,0 +1,18 @@
+<template>
+    <Label>Buy Price</Label>
+    <input type="number" >
+
+    <Label>Sell Price</Label>
+    <input type="number">
+
+</template>
+
+
+
+<script>
+
+export default{
+    name: "Calculator"
+}
+
+</script>
---
+++ frontend/src/views/CalculatorView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="calculatorContainer">
+        <Calculator />
+    </div>
+</template>
+
+<script>
+
+import Calculator from "../components/Calculator.vue"
+
+export default {
+    components: {
+        Calculator,
+    },
+}
+
+</script>
+
+
+
+<style scoped>
+
+.calculatorContainer{
+    display: flex;
+    justify-content: center;
+    background-color: var(--colour1);
+}
+
+
+</style>
