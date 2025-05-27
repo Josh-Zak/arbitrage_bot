14669075b166bcbe2ceab7f16dd3d200cd0a46fa
Josh Zak <joshuazak19@gmail.com>
2025-05-26 21:26:41 -0400

    May 26: Worked on calculator component

--- frontend/src/components/Calculator.vue
+++ frontend/src/components/Calculator.vue
@@ -1,37 +1,28 @@
 <template>
-    <div class="box">
-        <div class="input">
-            <Label for="buyPrice">Buy Price</Label>
-            <input type="number" v-model="buyPrice">
-        </div>
+    <div class="calculator-component-wrapper">
+        <h1 class="heading">Arbitrage Calculator</h1>
+        <h3 class="subheading">Calculate profit and loss based on your buy price, sell price, and amount of Bitcoin.</h3>
         
-        <div class="input">
-            <Label for="sellPrice">Sell Price</Label>
-            <input type="number" v-model="sellPrice">
-        </div>
+        <div class="box">
+            <div class="input">
+                <Label for="buyPrice">Buy Price</Label>
+                <input type="number" v-model="buyPrice">
+            </div>
+            
+            <div class="input">
+                <Label for="sellPrice">Sell Price</Label>
+                <input type="number" v-model="sellPrice">
+            </div>
 
-        <div class="input">
-            <Label for="amount">Amount</Label>
-            <input type="number" v-model="amount">
-        </div>
+            <div class="input">
+                <Label for="amount">Amount</Label>
+                <input type="number" v-model="amount">
+            </div>
 
-        <!-- Put a toggle on USD/BTC -->
-        <div class="toggle">
-            <button
-                class="btc-button"
-                :class="[btcSelected ? 'active' : '']"
-                @click="toggleCurrency"
-                >BTC</button>
-            <button
-                class="usd-button"
-                :class="[!btcSelected ? 'active' : '']"
-                @click="toggleCurrency"
-            >USD</button>
-        </div>
-
-        <div class="profit">    
-            <Label>Profit/Loss</Label>
-            <h3 id="profit">${{ getProfit }}</h3>
+            <div class="profit">    
+                <Label>Profit/Loss</Label>
+                <h3 id="profit">${{ getProfit }}</h3>
+            </div>
         </div>
     </div>
 
@@ -48,19 +39,16 @@ export default{
             buyPrice: null,
             sellPrice: null,
             amount: null,
-            btcSelected: true
         }
     },
     computed: {
         getProfit(){
-            return  (this.sellPrice - this.buyPrice) * this.amount;
+            if(this.buyPrice && this.sellPrice && this.amount){
+                return  ((this.sellPrice - this.buyPrice) * this.amount).toFixed(2);
+            }
+            return "0.00";
         }
     },
-    methods: {
-        toggleCurrency(){
-            this.btcSelected = !this.btcSelected;
-        }
-    }
 }
 
 </script>
@@ -68,9 +56,38 @@ export default{
 
 <style>
 
+/* Component wrapper */
+.calculator-component-wrapper{
+    margin-top: 60px;
+    margin-bottom: 60px;
+}
+
+
+/* Heading and subheading */
+.heading{
+    text-align: center;
+    color: var(--colour5);
+}
+
+.subheading{
+    text-align: center;
+    color: var(--colour5);
+    
+}
+
+
+/* Calculator box */
+.box {
+    border: 2px solid var(--colour3);
+    border-radius: 20px;
+    margin: 20px;
+}
+
+
 input {
     border-radius: 5px;
     border: none;
+    min-height: 100%;
 }
 
 .input, .profit{
@@ -80,45 +97,12 @@ input {
     gap: 10px;
     margin: 10px 20px;
     color: var(--colour5);
-    font-size: large;
+    font-size:x-large;
 }
 
 .profit {
     margin-top: 20px;
-}
-
-
-.box {
-    border: 2px solid var(--colour3);
-    border-radius: 20px;
-    margin: 20px;
-}
-
-.toggle{
-    margin: 20px;
-    text-align: right;
-}
-
-button{
-    padding: 10px 55px;
-}
-
-.toggle .active{
-    background-color: green;
-}
-
-.toggle button{
-    background-color: var(--colour2);
-}
-
-.btc-button{
-    border-top-left-radius: 8px;
-    border-bottom-left-radius: 8px;
-}
-
-.usd-button{
-    border-top-right-radius: 8px;
-    border-bottom-right-radius: 8px;
+    border-top: 1px solid var(--colour3);
 }
 
 </style>
