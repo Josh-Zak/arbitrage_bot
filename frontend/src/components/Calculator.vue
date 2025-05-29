35662a1408a45d28442bb82c2f776175b313a4d2
Josh Zak <joshuazak19@gmail.com>
2025-05-29 15:52:52 -0400

    May 29: Continued working on calculator page. Improved UI and CSS

--- frontend/src/components/Calculator.vue
+++ frontend/src/components/Calculator.vue
@@ -4,23 +4,32 @@
         <h3 class="subheading">Calculate profit and loss based on your buy price, sell price, and amount of Bitcoin.</h3>
         
         <div class="box">
-            <div class="input">
+            <div class="input-Field">
                 <Label for="buyPrice">Buy Price</Label>
-                <input type="number" v-model="buyPrice">
+                <div class="input-wrapper">
+                    <span class="dollar-sign">$</span>
+                    <input type="number" placeholder="0.00" v-model="buyPrice">
+                </div>
             </div>
             
-            <div class="input">
+            <div class="input-Field">
                 <Label for="sellPrice">Sell Price</Label>
-                <input type="number" v-model="sellPrice">
+                <div class="input-wrapper">
+                    <span class="dollar-sign">$</span>
+                    <input type="number" placeholder="0.00" v-model="sellPrice">
+                </div>
             </div>
 
-            <div class="input">
-                <Label for="amount">Amount</Label>
-                <input type="number" v-model="amount">
+            <div class="input-Field">
+                <Label for="amount">Bitcoin Amount</Label>
+                <div class="input-wrapper">
+                    <span class="dollar-sign">₿</span>
+                    <input type="number" placeholder="0" v-model="amount">
+                </div>
             </div>
 
             <div class="profit">    
-                <Label>Profit/Loss</Label>
+                <Label>Profit/Loss:</Label>
                 <h3 id="profit">${{ getProfit }}</h3>
             </div>
         </div>
@@ -79,29 +88,49 @@ export default{
 /* Calculator box */
 .box {
     border: 2px solid var(--colour3);
+    background-color: var(--colour2);
     border-radius: 20px;
     margin: 20px;
 }
 
-
-input {
-    border-radius: 5px;
-    border: none;
-    min-height: 100%;
+label{
+    display: block;
 }
 
-.input, .profit{
+.dollar-sign{
+    margin: 0px 10px;
+}
+
+.input-wrapper{
+    border-radius: 5px;
+    background-color: var(--colour1);
     display: flex;
-    align-items: center;
-    justify-content: space-between;
+    align-items: baseline;
+}
+
+input {
+    flex: 1;
+    border: none;
+    min-height: 50px;
+    background-color: transparent;
+    font-size: 90%;
+}
+
+.input-Field{
+    flex-direction: column;
+}
+
+.input-Field, .profit{
+    display: flex;
     gap: 10px;
-    margin: 10px 20px;
+    margin: 30px;
     color: var(--colour5);
-    font-size:x-large;
+    font-size: x-large;
 }
 
 .profit {
-    margin-top: 20px;
+    justify-content: space-between;
+    padding-top: 20px;
     border-top: 1px solid var(--colour3);
 }
 
